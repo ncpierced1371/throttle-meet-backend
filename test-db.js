99f1c0a86835fda980ec3c0dbe02d78147eec6f3
ncpierced1371 <ncpierced@yahoo.com>
2025-09-06 16:37:56 -0700

    Remove test database connection scripts
    Deleted test-db.cjs and test-db.js files containing sample PostgreSQL connection code, likely to clean up unused or sensitive test scripts.

--- test-db.js
+++
@@ -1,8 +0,0 @@
-const { Client } = require('pg');
-const client = new Client({
-  connectionString: 'postgresql://neondb_owner:npg_8YpFTUDng9Km@ep-jolly-cloud-ad9i63he-pooler.c-2.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
-});
-client.connect()
-  .then(() => console.log('Connected!'))
-  .catch(err => console.error('Connection error:', err))
-  .finally(() => client.end());
